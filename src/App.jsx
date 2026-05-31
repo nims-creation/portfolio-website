1a8a18e2cf4dc9201ba4ccb5bd0769c1882b07e4
Nims-Creation <nimscreation06@gmail.com>
2026-05-31 08:45:39 +0530

    feat: integrate ScrollProgress indicator into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -10,12 +9,14 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import ThankYou from './components/ThankYou';
 import Projects from './components/Projects';
+import ScrollProgress from './components/ScrollProgress';
 
 
 const App = () => {
   return (
     <Router>
-      <div className="bg-black min-h-screen font-sans">
+      <div className="bg-[#030712] min-h-screen font-sans">
+        <ScrollProgress />
         <Routes>
           <Route path="/" element={
             <>
